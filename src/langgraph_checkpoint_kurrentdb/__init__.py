8603124a83cda7dc5a6849e5b0dd818e857e43b2
Lougarou <yogisawesome@gmail.com>
2025-04-03 15:23:36 +0200

    draft tracing

--- src/langgraph_checkpoint_kurrentdb/__init__.py
+++ src/langgraph_checkpoint_kurrentdb/__init__.py
@@ -712,10 +712,69 @@ class KurrentDBSaver(BaseCheckpointSaver[str]):
             return str(0)
         return str(0)
 
+    """
+    Only support one level of subgraph at the moment
+    """
+    def trace(self, thread_id: str):
+        if self.client is None:
+            raise Exception("Synchronous Client is required.")
+        try:
+            checkpoints_events = self.client.get_stream(
+                stream_name="thread-" + str(thread_id),
+                resolve_links=True,
+                backwards=False  # read forwards
+            )
+            execution_times = []
+            previous_seen = None
+            previous_time = None
+            previous_step = None
+            for event in checkpoints_events:
+                if previous_time is None: #first event
+                    previous_time = event.recorded_at
 
-    #TODO: rework with new channel protocol
-    def trace(self, thread_id: int):
-        raise NotImplementedError("")
+                checkpoint = self.jsonplus_serde.loads(event.data)
+                metadata = self.jsonplus_serde.loads(event.metadata)
+                if previous_step is None:
+                    previous_step = metadata["step"]
+                tree = thread_id
+                if "langgraph_checkpoint_ns" in metadata and "langgraph_node" in metadata:
+                    tree = metadata["langgraph_node"]
+
+                if "channel_versions" in checkpoint:
+                    if previous_seen == None: #this is the first step
+                        previous_seen = {}
+
+                    added_node = {}
+                    for el in checkpoint["channel_versions"]:
+                        if el not in previous_seen or checkpoint["channel_versions"][el] != previous_seen[el]:
+                            node_name = ""
+                            if "writes" in metadata:
+                                if metadata["writes"] is None:
+                                    node_name = metadata["source"]
+                                else:
+                                    for key in metadata["writes"]:
+                                        node_name = key
+                                        break
+                            else:
+                                continue
+                            if node_name in added_node:
+                                continue
+                            added_node[node_name] = True
+                            delta = (event.recorded_at - previous_time)
+                            time_taken = delta.seconds * 1000 + delta.microseconds // 1000
+                            execution_times.append((tree, metadata["step"], event.recorded_at, node_name, time_taken))
+                            previous_seen[el] = checkpoint["channel_versions"][el]
+                if metadata["step"] != previous_step:
+                    previous_step = metadata["step"]
+                    previous_time = event.recorded_at
+            from langgraph_checkpoint_kurrentdb.tracing import print_tree
+            print_tree(thread_id, execution_times)
+            # print(execution_times)
+            #convert to tree
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
     def set_max_count(self, max_count: int, thread_id: int) -> None:
         raise NotImplementedError()
     def set_max_age(self, max_count: int, thread_id) -> None:
